9152d553e8720b4c04879543a9130c961385b0d4
Ashley Hutton <ashley.hutton@ku.edu>
2016-02-29 15:36:54 -0600

    add functionality of multiplication table in php

--- multiplication.php
+++ multiplication.php
@@ -1,3 +1,39 @@
 <?php
-	echo "Hello, world!";
+//Inside myfirstprogram.php
+function multiply($x, $y) {
+    $z = $x * $y;
+    return $z;
+}
+
+echo "<table>";
+
+	for ($x = 0; $x <= 100; $x++) {
+
+		if ($x == 0){
+			echo "<th> &nbsp </th>";
+		}
+		else{
+			echo " <th> $x </th> ";
+		}
+	}
+
+	for ($x = 1; $x <= 100; $x++) {
+
+		echo "<tr> <th> $x </th>";
+
+		for ($y = 1; $y <= 100; $y++) {
+
+			echo "<td> " . multiply($x, $y) . " </td>";
+
+		}
+		
+		echo "</tr>";
+
+	}
+
+
+
+echo "</table>";
+
+
 ?>
