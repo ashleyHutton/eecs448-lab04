da320937ecc1327854a91160c89fa8505869405e
Ashley Hutton <ashley.hutton@ku.edu>
2016-03-02 19:50:29 -0600

    fix submit button in html

--- quiz.php
+++ quiz.php
@@ -1,14 +1,14 @@
 <?php
 //access the global array called $_POST to get the values from the fields
-$q1c = $_POST["q1"];
-$q2c = $_POST["q2"];
-$q3c = $_POST["q3"];
-$q4c = $_POST["q4"];
-$q5c = $_POST["q5"];
+$q1 = $_POST["q1"];
+$q2 = $_POST["q2"];
+$q3 = $_POST["q3"];
+$q4 = $_POST["q4"];
+$q5 = $_POST["q5"];
 
 $numCorrect = 0;
 
-if (isset($_POST['submit'])){
+if (isset($_POST['fubmit'])){
 
 	if($q1 == "2") {
 		$numCorrect++;
@@ -29,27 +29,28 @@ if (isset($_POST['submit'])){
 
 	echo "Question 1: 1 + 1 = <br>";
 	echo "You answered: " .$q1. "<br>";
-	echo "Correct answer: 2.<br>";
+	echo "Correct answer: 2<br>";
 
 	echo "<br>Question 2: 12 - 12 = <br>";
 	echo "You answered: " .$q1. " <br>";
-	echo "Correct answer: 0.<br>";
+	echo "Correct answer: 0<br>";
 
 	echo "<br>Question 3: 0 - 12 = <br>";
 	echo "You answered: " .$q3. " <br>";
-	echo "Correct answer: -12.<br>";
+	echo "Correct answer: -12<br>";
 
 	echo "<br>Question 4: 670 + 8 = <br>";
 	echo "You answered: " .$q4. " <br>";
-	echo "Correct answer: 678.<br>";
+	echo "Correct answer: 678<br>";
 
 	echo "<br>Question 5: 1 - 2 = <br>";
 	echo "You answered: " .$q5. " <br>";
 	echo "Correct answer: -1<br>";
 
-	echo "Score: " .$numCorrect. "/5<br>";
+	echo "<br>Score: " .$numCorrect. "/5 ";
 
-	echo "= " .($correct/5)*100 "%";
+	echo "= " .($numCorrect/5)*100;
+	echo "%";
 }
 
 ?>
